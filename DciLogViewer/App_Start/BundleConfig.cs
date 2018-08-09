64aa2ad970bb12a81482676c660c2b851277f0a7
oefractal <oefractal@gmail.com>
2018-08-09 20:34:18 +0400

    Прикручивание jsGrid

--- DciLogViewer/App_Start/BundleConfig.cs
+++ DciLogViewer/App_Start/BundleConfig.cs
@@ -9,10 +9,15 @@ namespace DciLogViewer
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+        "~/Scripts/jquery-{version}.js"));
+      bundles.Add(new ScriptBundle("~/bundles/jsgrid").Include(
+        "~/Scripts/jsgrid.min.js"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/site.css"));
+        "~/Content/site.css"));
+      bundles.Add(new StyleBundle("~/Content/jsgrid").Include(
+        "~/Content/jsgrid-min.css",
+        "~/Content/jsgrid-theme.css"));
     }
   }
 }
